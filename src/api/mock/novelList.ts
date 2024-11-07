3f01e7e72b9494155a178768bd84969fbab2213a
jeong617 <loodol99@naver.com>
2024-11-07 11:27:56 +0900

    feat: mockdata file 추가 및 임시 default book cover image 추가

---
+++ src/api/mock/novelList.ts
@@ -0,0 +1,33 @@
+export const novelList =
+  {
+    'statusCode': 'NOVEL-LIST-200',
+    'message': '소설 목록을 성공적으로 조회했습니다.',
+    'totalElements': 100,
+    'totalPages': 10,
+    'currentPage': 1,
+    'size': 10,
+    'novelList': [
+      {
+
+        'id': '84e75c50-6d71-4607-b94f-97a70a36e444',
+        'title': '데미안',
+        'author': '헤르만 헤세',
+        'category': ['판타지', '모험'],
+        'coverImageUrl': 'https://example.com/images/cover1.jpg',
+      },
+      {
+        'id': '47ff7f74-27e3-48bd-8cf7-565d3bf7edab',
+        'title': '싯다르타',
+        'author': '헤르만 헤세',
+        'category': ['로맨스', '드라마'],
+        'coverImageUrl': 'https://example.com/images/cover2.jpg',
+      },
+      {
+        'id': '2a99959a-c04b-403c-a3f6-99e96d96fd7d',
+        'title': '햄릿',
+        'author': '윌리엄 셰익스피어',
+        'category': ['스릴러', '미스터리'],
+        'coverImageUrl': 'https://example.com/images/cover3.jpg',
+      },
+    ],
+  };
---
+++ src/api/mock/searchResult.ts
@@ -0,0 +1,24 @@
+export const searchResult = {
+  'statusCode': 'SEARCH-200',
+  'message': '헤르만 헤세와 일치하는 소설 목록을 성공적으로 조회했습니다.',
+  'totalElements': 10,
+  'totalPages': 1,
+  'currentPage': 1,
+  'size': 10,
+  'novelList': [
+    {
+      'id': '84e75c50-6d71-4607-b94f-97a70a36e444',
+      'title': '데미안',
+      'author': '헤르만 헤세',
+      'category': ['판타지', '모험'],
+      'coverImageUrl': 'https://example.com/images/cover1.jpg',
+    },
+    {
+      'id': '47ff7f74-27e3-48bd-8cf7-565d3bf7edab',
+      'title': '싯다르타',
+      'author': '헤르만 헤세',
+      'category': ['로맨스', '드라마'],
+      'coverImageUrl': 'https://example.com/images/cover2.jpg',
+    },
+  ],
+};
