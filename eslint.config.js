e017ac20c7ef8c568e1acc45b5b1c3e91b580eab
jeong617 <loodol99@naver.com>
2024-11-15 13:33:48 +0900

    chore: eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,31 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+    {
+        languageOptions: {
+            globals: globals.browser
+        },
+        settings: {
+            react: {
+                version: "detect", // React 버전을 자동 감지
+            },
+        },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ...pluginReact.configs.flat.recommended,
+        rules: {
+            ...pluginReact.configs.flat.recommended.rules,
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
 ];
