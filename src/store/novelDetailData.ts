be1855bb37df6451f16cb3d1deee4257555733ef
jeong617 <loodol99@naver.com>
2024-11-16 17:21:38 +0900

    refactor: api 응답 data interface 다른 파일로 분리

---
+++ src/store/novelDetailData.ts
@@ -0,0 +1,10 @@
+export interface NovelDetailData {
+    id: string;
+    title: string;
+    authorList: string[];
+    category: string[];
+    coverImageUrl: string;
+    summary: string;
+    views: number;
+    likes: number;
+};
