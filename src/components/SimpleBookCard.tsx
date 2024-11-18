49274019712a0e6904ec7043564250c75beb965b
jeong617 <loodol99@naver.com>
2024-11-18 10:54:12 +0900

    chore: default image 경로 변경

--- src/components/SimpleBookCard.tsx
+++ src/components/SimpleBookCard.tsx
@@ -1,29 +1,29 @@
-import React from "react";
+import React from 'react';
 
 interface SimpleBookCardProps {
-    title: string;
-    author: string;
-    coverImageUrl?: string;
+  title: string;
+  author: string;
+  coverImageUrl?: string;
 }
 
-function SimpleBookCard({ title, author, coverImageUrl }: SimpleBookCardProps) : React.JSX.Element {
-    const defaultImage = '/src/assets/book-cover/default-book-cover.jpg';
+function SimpleBookCard({ title, author, coverImageUrl }: SimpleBookCardProps): React.JSX.Element {
+  const defaultImage = '/public/book-cover/default-book-cover.jpg';
   const handleImageError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
     const target = e.target as HTMLImageElement;
     target.onerror = null;
     target.src = defaultImage;
   };
-    return (
-        <div className="flex flex-col items-start">
-            {/* 커버 이미지 */}
-            <img alt='cover-image' className="w-40 h-60 mb-2 shadow-lg rounded-normal-radius" src={coverImageUrl}
-                 onError={handleImageError}
-            />
-            {/* 제목 & 작가 */}
-            <div className="w-40 text-xl font-bold ml-1 truncate ...">{title}</div>
-            <div className="w-40 ml-1 text-ellipsis truncate ...">{author}</div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-start">
+      {/* 커버 이미지 */}
+      <img alt="cover-image" className="w-40 h-60 mb-2 shadow-lg rounded-normal-radius" src={coverImageUrl}
+           onError={handleImageError}
+      />
+      {/* 제목 & 작가 */}
+      <div className="w-40 text-xl font-bold ml-1 truncate ...">{title}</div>
+      <div className="w-40 ml-1 text-ellipsis truncate ...">{author}</div>
+    </div>
+  );
 }
 
 export default SimpleBookCard;
