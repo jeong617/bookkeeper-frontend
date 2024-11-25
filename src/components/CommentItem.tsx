7617c5924fc1be73e74340db05c0c02f2e63d0d2
jeong617 <loodol99@naver.com>
2024-11-25 17:08:49 +0900

    style: 백엔드와 변수명 통일

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -5,22 +5,22 @@ import { FaTrashAlt } from "react-icons/fa";
 import { Badge } from "flowbite-react";
 
 interface CommentItemProps {
-    chapter: number;
-    nickname: string;
-    comment: string;
+    episodeNumber: number;
+    userName: string;
+    content: string;
     createdAt: string;
 }
 
-function CommentItem({chapter, nickname, comment, createdAt}:CommentItemProps): React.JSX.Element {
+function CommentItem({episodeNumber, userName, content, createdAt}:CommentItemProps): React.JSX.Element {
 
     return (
         <div className="flex flex-col items-start bg-white mx-1 px-3 py-2 gap-1 rounded-normal-radius">
             {/* label */}
             <div className="w-full flex flex-row justify-between justify-center items-center gap-2">
                 <div className="flex flex-row gap-2">
-                    <Badge className="mb-1 rounded-chips-radius border border-button text-button" size="xs">{chapter}화</Badge>
+                    <Badge className="mb-1 rounded-chips-radius border border-button text-button" size="xs">{episodeNumber}화</Badge>
                     <div>
-                        <span className="mr-1">{nickname}</span>
+                        <span className="mr-1">{userName}</span>
                         <span className="text-xs">{createdAt}</span>
                     </div>
                 </div>
@@ -28,7 +28,7 @@ function CommentItem({chapter, nickname, comment, createdAt}:CommentItemProps): 
             </div>
 
             {/* comment */}
-            <span className="px-1.5">{comment}</span>
+            <span className="px-1.5">{content}</span>
         </div>
     )
 }
