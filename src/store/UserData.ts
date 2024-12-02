421b3292ec3dc26fcbef8d4b2e96c23a86fc4fa9
jeong617 <loodol99@naver.com>
2024-12-02 11:24:45 +0900

    chore: user data interface 수정

--- src/store/UserData.ts
+++ src/store/UserData.ts
@@ -1,6 +1,9 @@
+import { AgeGroupType } from './types.tsx';
+
 export interface UserData {
     email: string;
     password: string;
     name: string;
     gender: string,
+    ageGroup: AgeGroupType,
 };
