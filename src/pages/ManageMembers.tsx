f486515486d4f595c4e6293e3e3b0f18c7fcdfe6
jeong617 <loodol99@naver.com>
2024-12-08 23:13:57 +0900

    fix: param의 role이 바뀔 때마다 api 재요청

--- src/pages/ManageMembers.tsx
+++ src/pages/ManageMembers.tsx
@@ -5,112 +5,107 @@ import { Header } from '../components/header';
 import FormButton from '../components/FormButton.tsx';
 import SearchBar from '../components/SearchBar';
 import { RoleType, SortFieldType, SortDirectionType } from '../store/types.tsx';
+import { MemberData } from '../store/UserData.ts';
+import formatDateTime from '../utils/parseDateTime.ts';
 import { get } from '../api/api.ts';
 
 // css
 import { FaUser, FaUserCog, FaPlus } from 'react-icons/fa';
+import { FaUserCheck } from 'react-icons/fa6';
 import MemberItem from '../components/MemberItem.tsx';
 import { AxiosResponse } from 'axios';
 
 function ManageMembers(): React.JSX.Element {
-  const [activeTab, setActivetab] = useState<RoleType>(RoleType.Member);
   const [paramOption, setParamOptions] = useState({
     currentPage: 1,
     sortField: SortFieldType.CreatedAt,
     sortDirection: SortDirectionType.Desc,
     role: RoleType.Member,
   });
-  const [memberList, setMemberList] = useState([]);
-  const [adminList, setAdminList] = useState([]);
+  const [memberList, setMemberList] = useState<MemberData[]>([]);
 
-  // member list api
   useEffect(() => {
-    const getMemberList = async () => {
+    const getMemeberList = async () => {
       try {
         const res: AxiosResponse = await get({
-          url: `admin/members?page=${paramOption.currentPage}&size=10&sortField=${paramOption.sortField}&sortDirectioson=${paramOption.sortDirection}&role=${paramOption.role}`,
+          url: `api/admin/members?page=${paramOption.currentPage}&size=10&sortField=${paramOption.sortField}&sortDirectioson=${paramOption.sortDirection}&role=${paramOption.role}`,
         });
         setMemberList(res.data.memberList);
       } catch (error) {
         console.error('회원 목록을 가져오는 데 실패했습니다.', error);
       }
     };
-    getMemberList();
-  },)
-
-  // TODO: admin list api
-  useEffect(() => {
-    const getAdminList = async () => {
-      try {
-        const res: AxiosResponse = await get({
-          url: `admin/members?page=${paramOption.currentPage}&size=10&sortField=${paramOption.sortField}&sortDirectioson=${paramOption.sortDirection}`,
-        });
-        setAdminList(res.data.memberList);
-      } catch (error) {
-        console.error('회원 목록을 가져오는 데 실패했습니다.', error);
-      }
-    };
-    getAdminList();
-  },)
+    getMemeberList();
+  }, [paramOption.role]);
 
   return (
     <>
       <Header />
-      <div className="container px-48 mx-auto mt-12">
-        <div className="flex flex-row w-full h-14 justify-between">
-          <div className="flex flex-row mt-2">
+      <div className='container px-48 mx-auto mt-12'>
+        <div className='flex flex-row w-full h-14 justify-between'>
+          <div className='flex flex-row mt-2'>
             <button
-              type="button"
+              type='button'
               className={`w-32 font-bold rounded-t-normal-radius flex justify-center items-center gap-1
-                                ${activeTab === RoleType.Admin ? 'bg-background underline' : 'bg-white'}`}
-              onClick={() => setActivetab(RoleType.Admin)}
+                                ${paramOption.role === RoleType.Admin ? 'bg-background underline' : 'bg-white'}`}
+              onClick={() => setParamOptions((prev) => ({ ...prev, role: RoleType.Admin }))}
             >
               <FaUser />
               ADMINS
             </button>
             <button
-              type="button"
+              type='button'
               className={`w-32 font-bold rounded-t-normal-radius flex justify-center items-center gap-1
-                                ${activeTab === RoleType.Member ? 'bg-background underline' : 'bg-white'}`}
-              onClick={() => setActivetab(RoleType.Member)}
+                                ${paramOption.role === RoleType.Member ? 'bg-background underline' : 'bg-white'}`}
+              onClick={() => setParamOptions((prev) => ({ ...prev, role: RoleType.Member }))}
             >
-              <FaUserCog />
+              <FaUserCog size={20} />
               MEMBERS
             </button>
+            <button
+              type='button'
+              className={`w-32 font-bold rounded-t-normal-radius flex justify-center items-center gap-1
+                                ${paramOption.role === RoleType.PendingAdmin ? 'bg-background underline' : 'bg-white'}`}
+              onClick={() => setParamOptions((prev) => ({ ...prev, role: RoleType.PendingAdmin }))}
+            >
+              <FaUserCheck size={20} />
+              승인 대기
+            </button>
           </div>
-          <div className="flex flex-row items-center pr-2 mb-2">
-            <div className="scale-75">
+          <div className='flex flex-row items-center pr-2 mb-2'>
+            <div className='scale-75'>
               <SearchBar />
             </div>
             <FormButton
-              label="ADD"
-              icon={<FaPlus className="fill-button-text" />}
+              label='ADD'
+              icon={<FaPlus className='fill-button-text' />}
             />
           </div>
         </div>
 
-        {/* admin & member list */}
-        <div className="bg-background w-full py-3 rounded-tr-normal-radius rounded-b-normal-radius">
-          {activeTab === RoleType.Member && (
-            <>
-              <div className="grid grid-cols-8 gap-2 px-2 items-center mx-2 pb-1">
-                <span className="justify-self-center">프로필</span>
-                <span className="col-span-2">닉네임</span>
-                <span className="col-span-2">이메일</span>
-                <span className="col-span-2">가입일</span>
-                <span>활성여부</span>
-              </div>
-              <div className="divide-y">
+        {/* admin & pending admin & member list */}
+        <div className='bg-background w-full py-3 rounded-tr-normal-radius rounded-b-normal-radius'>
+          <>
+            <div className='grid grid-cols-8 gap-2 px-2 items-center mx-2 pb-1'>
+              <span className='justify-self-center'>프로필</span>
+              <span className='col-span-2'>닉네임</span>
+              <span className='col-span-2'>이메일</span>
+              <span className='col-span-2'>가입일</span>
+              <span className='mx-auto'>활성여부</span>
+            </div>
+            <div className='divide-y'>
+              {memberList.map(member => (
                 <MemberItem
-                  email="crystal@gmail.com"
-                  createdAt="2024-06-17"
-                  nickname="^^"
-                  isAccountActive={true}
-                  className="rounded-t-normal-radius"
+                  key={member.id}
+                  email={member.email}
+                  createdAt={formatDateTime(member.createdAt)}
+                  nickname={member.nickname}
+                  profile={member.profileImageUrl}
+                  isAccountActive={member.deletedAt}
                 />
-              </div>
-            </>
-          )}
+              ))}
+            </div>
+          </>
         </div>
       </div>
     </>
