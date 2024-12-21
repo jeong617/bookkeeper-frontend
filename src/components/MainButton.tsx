8dfc99868ab8358a1875ccc7eb4cf33af98bd916
jeong617 <loodol99@naver.com>
2024-12-21 18:10:28 +0900

    feat: button component에 onClick props 추가

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 interface MainButtonProps {
     className?: string;
+    onClick?: () => void;
     label: string;
 }
 
-function MainButton({label, className}: MainButtonProps): React.JSX.Element {
+function MainButton({label, onClick, className}: MainButtonProps): React.JSX.Element {
     return (
-        <div className={`px-3 py-1.5 bg-button rounded-[10px] shadow justify-center items-center gap-2.5 inline-flex ${className}`}>
+        <div className={`px-3 py-1.5 bg-button rounded-[10px] shadow justify-center items-center gap-2.5 inline-flex ${className}`}
+             onClick={onClick}
+        >
             <div className="text-center text-white font-bold">{label}</div>
         </div>
     )
