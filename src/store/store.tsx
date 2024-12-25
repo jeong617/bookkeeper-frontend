16976b18bba0779b4f5671f25258647964b9d73e
jeong617 <loodol99@naver.com>
2024-12-26 06:42:22 +0900

    feat: tts 변환중 file 목록 localStorage에 반영

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -16,6 +16,7 @@ interface FileUploadData {
 interface FileStore {
   files: FileUploadData[];
   addFile: (file: FileUploadData) => void;
+  loadFiles: () => void;
   updateFileStatus: (id: string, status: TTSUploadStatusType) => void;
   getFilesByStatus: (status: TTSUploadStatusType) => FileUploadData[];
 }
@@ -30,16 +31,25 @@ export const useSideBarStore = create<SideBarState>((set) => ({
 }))
 
 export const useFileStore = create<FileStore>((set, get) => ({
-  files: [],
-  addFile: (file) =>
-    set((state) => ({
-      files: [...state.files, file],
-    })),
+  files: JSON.parse(localStorage.getItem('files') || '[]'),
+  addFile: (file) => {
+    set((state) => {
+      const updatedFiles = [...state.files, file];
+      localStorage.setItem('files', JSON.stringify(updatedFiles)); // 로컬 스토리지에 저장
+      return { files: updatedFiles };
+    });
+  },
+  loadFiles: () => {
+    const storedFiles = JSON.parse(localStorage.getItem('files') || '[]');
+    set({ files: storedFiles });
+  },
   updateFileStatus: (id, status) =>
-    set((state) => ({
-      files: state.files.map((file) =>
+    set((state) => {
+      const updatedFiles = state.files.map((file) =>
         file.id === id ? { ...file, status } : file
-      ),
-    })),
+      );
+      localStorage.setItem('files', JSON.stringify(updatedFiles)); // 변경된 상태를 localStorage에 반영
+      return { files: updatedFiles };
+    }),
   getFilesByStatus: (status) => get().files.filter((file) => file.status === status),
 }));
