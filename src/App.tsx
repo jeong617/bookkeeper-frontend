a307eab68bf614a5cfc38348da94d6cb995f47c8
jeong617 <loodol99@naver.com>
2025-01-26 01:31:58 +0900

    chore: react query devtool 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/Routes.tsx';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <RouterProvider router={router} />
+  </QueryClientProvider>
 );
