38ffd2de903cecaa9f18de279d9c824eff35a138
jeong617 <loodol99@naver.com>
2025-01-25 23:36:41 +0900

    feat: react query 초기 setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import "./App.css";
-import DashBoard from "./pages/DashBoard.tsx";
+import { createRoot } from 'react-dom/client'
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Routes.tsx';
 
-function App() {
-
-  return (
-    <>
-      <DashBoard />
-    </>
-  );
-}
-
-export default App;
+createRoot(document.getElementById('root')!).render(
+    <RouterProvider router={router} />
+);
